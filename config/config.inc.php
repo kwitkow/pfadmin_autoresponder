4db70a685e34ab23cad933e97db463cc6e9f2d7c
wackerm <[omitted]>
2020-03-16 14:19:36 +0100

    add sample configuration

--- config/config.inc.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-#$rcmail_config['db_pfadmin_autoresponder_dsn'] = 'mysql://postfixadmin:XEAPerlJ2XAJ@localhost/postfixadmin';
-$rcmail_config['db_pfadmin_autoresponder_dsn'] = 'mysql://mail_vacation:Bdv6zedKtaw7v9E3@10.5.5.50/system';
-$rcmail_config['vac_domain']='autoreply.vacations.kreapptivo.de';
-
-?>
